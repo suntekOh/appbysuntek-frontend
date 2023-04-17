2ba44b750293fd91360fcd205ff3f3e453f796fa
Suntek Oh <suntekoh@outlook.com>
2023-04-17 19:25:51 -0400

    revert axios withCredentials setting

--- src/services/http-client-service.ts
+++ src/services/http-client-service.ts
@@ -4,7 +4,7 @@ export const createHttpClient = () => {
     const baseURL = process.env.REACT_APP_ENV == 'development' ? process.env.REACT_APP_BACKEND_BASEURL_LOCAL : process.env.REACT_APP_BACKEND_BASEURL;
     return axios.create({
         baseURL: baseURL,
-        withCredentials: false
+        withCredentials: true
     }); 
 }
 
